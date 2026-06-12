b4ff138a7687dfe288c8463a92fbbef71ade60c2
Андрей Атаманов <rar3065003@gmail.com>
2026-06-12 22:31:03 +0700

    5-th ex half made

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,24 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+
+    name = models.CharField(max_length = 100)
+
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+
+    name = models.CharField(max_length = 100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
--- config/settings.py
+++ config/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'skypro_catalog',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
