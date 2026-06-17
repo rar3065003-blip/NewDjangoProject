ded20c0c8bf2f120afd90b1fee95c16608c1ac56
Андрей Атаманов <rar3065003@gmail.com>
2026-06-17 21:47:46 +0700

    complite 3-th lesson

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_view, contacts_view
+from .views import home_view, contacts_view, product_detail
 
 urlpatterns = [
     path("", home_view, name="home"),
     path("contacts/", contacts_view, name="contacts"),
+    path("product/<int:pk>/", product_detail, name="product_detail"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from catalog.models import Product
 
 def home_view(request):
     latest_products = Product.objects.order_by('-created_at')[:5]
     print(latest_products)
-    return render(request, "catalog/home.html")
+    context = {'products': latest_products}
+    return render(request, "catalog/home.html", context)
 
 
 def contacts_view(request):
     return render(request, "catalog/contacts.html")
+
+
+def product_detail(request, pk):
+    """Контроллер для отображения детальной информации о товаре"""
+    # Находим товар по его первичному ключу (id) или возвращаем ошибку 404
+    product = get_object_or_404(Product, pk=pk)
+
+    # Передаем объект товара в шаблон
+    context = {
+        'object': product  # В курсах часто называют переменную 'object' или 'product'
+    }
+    return render(request, 'catalog/product_detail.html', context)
