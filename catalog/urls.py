cd9118589968f78ce5b92d0c04eeb7a8a47d4718
Андрей Атаманов <rar3065003@gmail.com>
2026-08-09 19:48:45 +0700

    Напонение функционала закончено 09.08.26

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
-from .views import ContactsView, HomeView, ProductDetailView
+from .views import (
+    ContactsView,
+    HomeView,
+    ProductCreateView,
+    ProductDetailView,
+    ProductUpdateView,
+)
+
+app_name = "catalog"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("contacts/", ContactsView.as_view(), name="contacts"),
     path("products/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("create/", ProductCreateView.as_view(), name="create_product"),
+    path("update/<int:pk>/", ProductUpdateView.as_view(), name="update_product"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import get_object_or_404, render
-from django.views.generic import DetailView, ListView, TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    TemplateView,
+    UpdateView,
+)
 
+from catalog.forms import ProductForm
 from catalog.models import Product
 
 
@@ -10,7 +18,6 @@ class HomeView(ListView):
     context_object_name = "products"
 
 
-
 class ContactsView(TemplateView):
     template_name = "catalog/contacts.html"
 
@@ -18,3 +25,23 @@ class ContactsView(TemplateView):
 class ProductDetailView(DetailView):
     model = Product
     template_name = "catalog/product_detail.html"
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:home")
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:home")
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = "catalog/product_form.html"
+    success_url = reverse_lazy("catalog:home")
